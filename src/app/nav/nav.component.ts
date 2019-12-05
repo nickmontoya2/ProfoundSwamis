c30eb8bccdbef5aac4e6c09b854642e77966b67e
Marc Marti <helloimmarc@gmail.com>
2019-12-05 15:17:14 -0500

    working nav

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+// import * as $ from 'jquery';
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
+
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.body.style.backgroundColor = 'white';
+  }
+
+}
