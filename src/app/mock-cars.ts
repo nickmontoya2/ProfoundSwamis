39fd1d5ac245b8ac027cb1ca81fe70a642879d3f
nickmontoya2 <nickmontoya2@gmail.com>
2019-12-05 12:58:16 -0500

    Basic layout for multi-car view

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -36,4 +36,40 @@ export const MOCK_CARS: Car[] = [{
   transmission: 'automatic',
   description: 'Made to haul',
   status: 1
+}, {
+  carId: 4,
+  make: 'Chevy',
+  model: 'Camaro',
+  year: 2010,
+  value: 18000,
+  mileage: 42350,
+  fuel: 'gasoline',
+  color: 'Blue',
+  transmission: 'automatic',
+  description: 'Dope Camaro goes woo woo',
+  status: 1
+}, {
+  carId: 5,
+  make: 'Kia',
+  model: 'Soul',
+  year: 2011,
+  value: 12000,
+  mileage: 70000,
+  fuel: 'gasoline',
+  color: 'Silver',
+  transmission: 'automatic',
+  description: 'Beautiful hamster car',
+  status: 1
+}, {
+  carId: 6,
+  make: 'Chevy',
+  model: 'Silverado',
+  year: 2015,
+  value: 22000,
+  mileage: 35000,
+  fuel: 'gasoline',
+  color: 'Black',
+  transmission: 'automatic',
+  description: 'Made to haul',
+  status: 1
 }];
